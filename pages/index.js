eacac664e0536d6406e9ddc1ff142721a5f7e15f
Ophtanass <chadli.nassim@gmail.com>
2025-09-22 21:55:08 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <img
+        src="/background-flat-girl-lille.png"
+        alt="Image test"
+        className="w-[800px] h-auto border-4 border-red-500"
+      />
+    </div>
+  );
+}
